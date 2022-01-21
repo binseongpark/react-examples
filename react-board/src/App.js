c6d69a08c7ca1433a294b3af6c338ab72ab77688
박성빈 <kled@kakao.com>
2022-01-21 13:41:36 +0900

    fix .

--- react-board/src/App.js
+++ react-board/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import styled, { css } from "styled-components";
-import { Table } from 'react-bootstrap'
+import { Button, Table } from 'react-bootstrap'
 
 
 function App() {
@@ -12,35 +12,41 @@ function App() {
         <img src={'/favicon.png'} alt="logo" />
       </header>
       <BodyWrapper>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Username</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td colSpan={2}>Larry the Bird</td>
-              <td>@twitter</td>
-            </tr>
-          </tbody>
-        </Table>
+        <div>
+          <Button>글쓰기</Button>
+        </div>
+        <TableContainer>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Username</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td colSpan={2}>Larry the Bird</td>
+                <td>@twitter</td>
+              </tr>
+            </tbody>
+          </Table>
+        </TableContainer>
+
       </BodyWrapper>
     </div>
   );
@@ -48,6 +54,11 @@ function App() {
 
 const BodyWrapper = styled.div`
   padding: 24px;
+`
+
+const TableContainer = styled.div`
+  margin-top: 1rem;
+  marginb-bottom: 1rem;
 `
 
 export default App;
